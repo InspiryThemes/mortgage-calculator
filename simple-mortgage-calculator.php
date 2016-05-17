fee18badc6578bdfbe8c3e9c4ff161ce88f15d18
sungraizfaryad <sungraiz@inspirythemes.com>
2016-05-17 14:20:37 +0500

    Basic Structure added

---
+++ simple-mortgage-calculator.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:      Simple Mortgage Calculator
+ * Plugin URI:        https://github.com/saqibsarwar/quick-and-easy-tweets
+ * Description:       It is a very Simple calculator for Mortgage Calculations
+ * Version:           1.0.0
+ * Author:            M Saqib Sarwar
+ * Author URI:        http://saqibsarwar.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       qaet
+ * Domain Path:       /languages
+ */
