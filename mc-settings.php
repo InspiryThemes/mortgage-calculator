38cbe5d6e6a3779d97397f6030b23987ff3de0dd
sungraizfaryad <sungraiz@inspirythemes.com>
2018-03-21 11:06:42 +0500

    Setting API Class Prefixed

--- mc-settings.php
+++ mc-settings.php
@@ -9,7 +9,7 @@ if ( !class_exists('MC_Mortgage_Calculator_Settings' ) ):
 		private $settings_api;
 
 		function __construct() {
-			$this->settings_api = new WeDevs_Settings_API;
+			$this->settings_api = new MC_WeDevs_Settings_API;
 
 			add_action( 'admin_init', array($this, 'admin_init') );
 			add_action( 'admin_menu', array($this, 'admin_menu') );
